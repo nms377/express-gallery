3be45287de8d075e9fc9b97147ee577b7b7e7b31
nms377 <nms377@nyu.edu>
2017-02-11 16:00:40 -1000

    Setup POST request on /.

--- routes/gallery-route.js
+++ routes/gallery-route.js
@@ -6,17 +6,27 @@ const models = require('../models');
 const app = express();
 const router = express.Router();
 
-
+//	Gallery Index
 router.route('/')
 	.get((req, res) => {
-
 		models.Gallery.findAll()
 			.then(function (gallery) {
 				// res.send('sanity');
 				res.json(gallery);
 			});
+})
+	.post((req, res) => {
+		models.Gallery.create({ 
+		author: req.body.author,
+		link: encodeURI(req.body.link),
+		description: req.body.description
+	 })
+	.then(function(gallery) {
+		res.json(gallery);
+	});
 });
 
+//	Gallery Index By id
 // app.get('/gallery/:id', (req, res) => {
 // 	Gallery.findById()
 // 	.then(function (gallery) {
