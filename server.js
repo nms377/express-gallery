93da8c369c2d1218486a9e38e6b0d30d6c3ecdb5
nms377 <nms377@nyu.edu>
2017-02-11 15:34:55 -1000

    Moved my requests to routes and imported routes into server.

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ const app = express();
 
 app.use( bp.urlencoded({extended: true}));
 
+const gallery = require('./routes/gallery-route');
 const db = require('./models');
 const { User } = db;	//	object destructuring
 const { Gallery } = db;
 
-app.get('/gallery', (req, res) => {
-	Gallery.findAll()
-		.then(function (gallery) {
-			res.json(gallery);
-		});
-});
+//	route
+app.use('/gallery', gallery );
 
 app.listen(3000, function() {
 	console.log('Server listening on port 3000');
 	db.sequelize.sync();
 });
+
+module.exports = app;
