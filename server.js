7dc09a176d865d83ffa4bb2c99afd1fb2cc15a0c
nms377 <nms377@nyu.edu>
2017-02-16 21:20:09 -1000

    Imported passport, config, and express-session middlewares. Included password authentication and serialize/deserialize.

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const bp = require('body-parser');
 const app = express();
 const handlebars = require('express-handlebars');
 const methodOverride = require('method-override');
-
 const hbs = handlebars.create({
 	extname: '.hbs',
 	defaultLayout: 'app'
 });
+
+//	password middleware for User
+const CONFIG = require('./config/config.json');
+const	LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const passport = require('passport');
 
 //	setup handlebars
 app.engine('hbs', hbs.engine);
@@ -19,8 +24,18 @@ app.use( bp.urlencoded({extended: true}));
 //	method-override
 app.use(methodOverride('_method'));
 
+//	express-session
+app.use(session({
+	secret: CONFIG.SESSION_SECRET
+}));
+
+//	this goes after every other middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //	routes
 const gallery = require('./routes/gallery-route');
+const user = require('./routes/user-route');
 
 //	models
 const db = require('./models');
@@ -29,7 +44,50 @@ const { Gallery } = db;
 
 //	route
 app.use('/gallery', gallery );
+app.use('/user', user );
 
+//	authenticate password
+const authenticate = (username, password) => {
+  // get user data from the DB
+  const { USERNAME } = CONFIG;
+  const { PASSWORD } = CONFIG;
+
+ // check if the user is authenticated or not
+  return ( username === USERNAME && password === PASSWORD );
+};
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    console.log('username, password: ', username, password);
+    // check if the user is authenticated or not
+    if( authenticate(username, password) ) {
+    	console.log('passed');
+
+      // User data from the DB
+      const user = {
+        name: 'Joe',
+        role: 'admin',
+        favColor: 'green',
+        isAdmin: true,
+      };
+
+      return done(null, user); // no error, and data = user
+    }
+    return done(null, false); // error and authenticted = false
+  }
+));
+
+//	serialize user
+passport.serializeUser(function(user, done) {
+	return done(null, user);
+});
+
+//	deserialize user
+passport.deserializeUser(function(user, done) {
+	return done(null, user);
+});
+
+//	listening on port
 app.listen(3000, function() {
 	console.log('Server listening on port 3000');
 	db.sequelize.sync();
