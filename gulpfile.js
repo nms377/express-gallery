4062430aa1b705d1db68478e622abccc11ebd3a4
nms377 <nms377@nyu.edu>
2017-02-18 12:29:32 -1000

    Setup gulp file. Removed browser-synce from my task watch file because it created a new port.

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const scss = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+//	Compile sass
+gulp.task('scss', () => {
+	return gulp.src('./scss/*.scss')
+	.pipe(scss({ errLogToConsole: true }))
+	.pipe(gulp.dest('./public/css'))
+})
+
+gulp.task('browserSync', () => {
+	return browserSync.init(['./public'], {
+		server: {
+			baseDir: './public'
+		}
+	})
+})
+
+//	watch files for changes
+gulp.task('watch', () => {
+	gulp.watch('./scss/**/*.scss', ['scss'])
+})
+
+//	default task
+gulp.task('default', ['scss', 'watch'])
