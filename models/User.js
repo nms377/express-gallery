a334846bf4fcc8fa790297f7ad2ddaae96b7100b
nms377 <nms377@nyu.edu>
2017-02-22 19:57:18 -1000

    Updating User model to include columns password, role, favColor, and isAdmin.

--- models/User.js
+++ models/User.js
@@ -1,6 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
 	var User = sequelize.define("User", {
-		username: DataTypes.STRING
+		username: DataTypes.STRING,
+		password: DataTypes.STRING,
+		role: DataTypes.STRING,
+		favColor: DataTypes.STRING,
+		isAdmin: DataTypes.BOOLEAN
 	}, {
 		classMethods: {
 			associate: function(models) {
