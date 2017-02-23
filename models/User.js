449f57089a90fe62702e29ed39b59d4c39cbe369
nms377 <nms377@nyu.edu>
2017-02-22 22:07:58 -1000

    Updated the table columns.

--- models/User.js
+++ models/User.js
@@ -2,9 +2,6 @@ module.exports = function(sequelize, DataTypes) {
 	var User = sequelize.define("User", {
 		username: DataTypes.STRING,
 		password: DataTypes.STRING,
-		role: DataTypes.STRING,
-		favColor: DataTypes.STRING,
-		isAdmin: DataTypes.BOOLEAN
 	}, {
 		classMethods: {
 			associate: function(models) {
