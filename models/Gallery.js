56fbaab9a1d7710b3bb99373aefb2e080c797d75
nms377 <nms377@nyu.edu>
2017-02-25 11:37:00 -1000

    installed sequelize-init.

--- models/Gallery.js
+++ models/Gallery.js
@@ -1,15 +1,16 @@
+'use strict';
 module.exports = function(sequelize, DataTypes) {
-	var Gallery = sequelize.define("Gallery", {
-		author: DataTypes.STRING,
-		link: DataTypes.STRING,
-		description: DataTypes.STRING
-	},{
-		classMethods: {
-			associate: function(models) {
-				Gallery.belongsTo(models.User);
-			}
-		}
-	});
-
-	return Gallery;
+  var Gallery = sequelize.define('Gallery', {
+    author: DataTypes.STRING,
+    link: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+        Gallery.belongsTo(models.User);
+      }
+    }
+  });
+  return Gallery;
 };
